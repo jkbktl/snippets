e8248d2ef192da365288deef98483cd1f425db8b
Jakub Kotula <jakub.kotula@goodshape.cz>
2014-04-14 13:41:55 +0200

    Slideshow snippet - basic functions

---
+++ slideshow/slideshow.coffee
@@ -0,0 +1,72 @@
+# To-do
+# - add paging
+# - destroy method
+
+class Slideshow
+  state: null
+  autoSpeed: 2000
+  
+  sliding: false
+  step: 200
+
+  constructor: (settings = {}) ->
+
+    @root = settings.root or $ '.slideshow-wrapper'
+    @item = settings.item or $ '.slideshow-wrapper .slideshow-item'
+    @prev = settings.prev or $ '<a class="prev" href="#" />'
+    @next = settings.next or $ '<a class="next" href="#" />'
+    @lift = settings.lift or ($ '.slideshow-wrapper .slideshow-lift')
+    @paging = settings.paging or ($ '<div class="paging" />').appendTo @root
+
+    console.log @root
+
+    (@prev.appendTo @root).click (e) =>
+      e.preventDefault()
+      @slideMe 'prev'
+
+    (@next.appendTo @root).click (e) =>
+      e.preventDefault()
+      @slideMe 'next'
+
+  
+  slideMe: (dir) ->
+    if(@sliding == true)
+      return
+
+    @unsetAutomat()
+    last = @item.filter(':last-child')
+    first = @item.filter(':first-child')
+
+    @sliding = true
+
+    switch dir
+      when "prev"
+        last.prependTo(@lift)
+        @lift.css {'left': @step * -1}
+        @lift.animate {left: 0}, 300, =>
+          @sliding = false
+          @setAutomat()
+
+      when "next"
+        @lift.animate {left: @step * -1}, 300, =>
+          first.appendTo(@lift)
+          @lift.css {'left': 0}
+          @sliding = false
+          @setAutomat()
+
+  destroy: ->
+    @root.hide
+    @unsetAutomat
+
+  setAutomat: ->
+    @automat = setTimeout =>
+      @slideMe('next')
+    , @autoSpeed
+
+  unsetAutomat: ->
+    clearTimeout @automat
+
+
+$('document').ready ->
+  doSlideshow = new Slideshow({
+  })
---
+++ slideshow/slideshow.js
@@ -0,0 +1,96 @@
+(function() {
+  var Slideshow;
+
+  Slideshow = (function() {
+    Slideshow.prototype.state = null;
+
+    Slideshow.prototype.autoSpeed = 2000;
+
+    Slideshow.prototype.sliding = false;
+
+    Slideshow.prototype.step = 200;
+
+    function Slideshow(settings) {
+      var _this = this;
+      if (settings == null) {
+        settings = {};
+      }
+      this.root = settings.root || $('.slideshow-wrapper');
+      this.item = settings.item || $('.slideshow-wrapper .slideshow-item');
+      this.prev = settings.prev || $('<a class="prev" href="#" />');
+      this.next = settings.next || $('<a class="next" href="#" />');
+      this.lift = settings.lift || ($('.slideshow-wrapper .slideshow-lift'));
+      this.paging = settings.paging || ($('<div class="paging" />')).appendTo(this.root);
+      console.log(this.root);
+      (this.prev.appendTo(this.root)).click(function(e) {
+        e.preventDefault();
+        return _this.slideMe('prev');
+      });
+      (this.next.appendTo(this.root)).click(function(e) {
+        e.preventDefault();
+        return _this.slideMe('next');
+      });
+    }
+
+    Slideshow.prototype.slideMe = function(dir) {
+      var first, last,
+        _this = this;
+      if (this.sliding === true) {
+        return;
+      }
+      this.unsetAutomat();
+      last = this.item.filter(':last-child');
+      first = this.item.filter(':first-child');
+      this.sliding = true;
+      switch (dir) {
+        case "prev":
+          last.prependTo(this.lift);
+          this.lift.css({
+            'left': this.step * -1
+          });
+          return this.lift.animate({
+            left: 0
+          }, 300, function() {
+            _this.sliding = false;
+            return _this.setAutomat();
+          });
+        case "next":
+          return this.lift.animate({
+            left: this.step * -1
+          }, 300, function() {
+            first.appendTo(_this.lift);
+            _this.lift.css({
+              'left': 0
+            });
+            _this.sliding = false;
+            return _this.setAutomat();
+          });
+      }
+    };
+
+    Slideshow.prototype.destroy = function() {
+      this.root.hide;
+      return this.unsetAutomat;
+    };
+
+    Slideshow.prototype.setAutomat = function() {
+      var _this = this;
+      return this.automat = setTimeout(function() {
+        return _this.slideMe('next');
+      }, this.autoSpeed);
+    };
+
+    Slideshow.prototype.unsetAutomat = function() {
+      return clearTimeout(this.automat);
+    };
+
+    return Slideshow;
+
+  })();
+
+  $('document').ready(function() {
+    var doSlideshow;
+    return doSlideshow = new Slideshow({});
+  });
+
+}).call(this);
